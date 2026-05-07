61173ba63b6911854d39e293408f04174ac191d9
sakura-0405 <godson950405@gmail.com>
2026-05-07 12:05:08 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -283,7 +283,7 @@ def search():
 
 @app.route("/road")
 def road(): 
-    Ro = "<h1>台中市十大肇事路口(113年10月)</h1><br>"
+    Ro = "<h1>台中市十大肇事路口(113年10月) 作者 : 徐瑞穎</h1><br>"
     url = "https://datacenter.taichung.gov.tw/swagger/OpenData/a1b899c0-511f-4e3d-b22b-814982a97e41"
     Data = requests.get(url)
 
